fb7e3866aa255d13e7951521742adc08511af9b4
Jyotiram-Nayak <jyotiramnayak7600@gmail.com>
2025-04-28 16:39:41 +0530

    Setup GitHub Actions for auto deploy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    nextConfig,
+    output: 'export',
+    images: { unoptimized: true },
+    basePath: '/foodieswebsite',    
+    assetPrefix: '/foodieswebsite/', 
+  };
